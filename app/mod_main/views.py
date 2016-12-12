83d2b8817f2eac1367e3b9a79c280820ed8df064
Lorina Morina <loriinna.m@gmail.com>
2016-12-12 21:46:29 +0000

    New changes

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,11 +1,35 @@
-from flask import Blueprint
 
+from flask import Blueprint, render_template, request
+from bson import json_util
+from app import mongo
 mod_main = Blueprint('main', __name__)
 
-
-@mod_main.route('/', methods=['GET'])
+@mod_main.route('/', methods=['GET','POST'])
 def index():
-    ''' Renders the App index page.
-    :return:
-    '''
-    return "Welcome to the Flask App."
+   ''' Renders the App index page.
+   :return:
+   '''
+   db = mongo.db.arkep
+
+   if request.method == 'GET':
+       return render_template('index.html')
+   elif request.method == 'POST':
+       data = request.form.to_dict()
+       db.insert(data)
+       return json_util.dumbs(data)
+   else:
+       return 'Bad request'
+
+@mod_main.route('/<string:id>', methods=['GET'])
+def get_doc(id):
+
+   #Return a mongo document with the specified id.
+   #return MongoDb Cursor
+
+   db = mongo.db.arkep
+
+   if request.method == 'GET':
+        doc = db.find({"_id":Object (id)})
+        return "You requested a document with ID" + id
+   else:
+        return 'Bad request'
